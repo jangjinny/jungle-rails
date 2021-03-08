3b316250a2240d99eec6b052182c077323f26fb8
Jinny Jang <jangjinny21@gmail.com>
2021-03-08 20:09:46 +0000

    add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,21 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  resources :users, only: [:index, :new, :create]
+  
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
