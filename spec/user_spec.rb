789dec36d2ea2b4a3d12034e054316cfe1c6014f
Jinny Jang <jangjinny21@gmail.com>
2021-03-09 06:39:55 +0000

    add tests for password and validation specs

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -15,15 +15,28 @@ RSpec.describe Product, type: :model do
   it 'is not valid without a unique email' do
     @user1 = User.create(first_name: 'Phoebe', last_name: 'Buffay', email: 'email@email.com', password: 'password', password_confirmation: 'password')
     @user2 = User.create(first_name: 'Phoebe', last_name: 'Buffay', email: 'EMAIL@EMAIL.com', password: 'password', password_confirmation: 'password')
-
     expect(@user2).not_to be_valid
     @user2.errors.full_messages
   end
+  it 'is not valid without an email' do
+    @user = User.new(first_name: 'Rachel', last_name: 'Green', email: nil, password: 'different', password_confirmation: 'password')
+    expect(@user).not_to be_valid
+    @user.errors.full_messages
+  end
+  it 'is not valid without a first_name' do
+    @user = User.new(first_name: nil, last_name: 'Green', email: 'email@email.com', password: 'different', password_confirmation: 'password')
+    expect(@user).not_to be_valid
+    @user.errors.full_messages
+  end
+  it 'is not valid without a last name' do
+    @user = User.new(first_name: 'Rachel', last_name: nil, email: 'email@email.com', password: 'different', password_confirmation: 'password')
+    expect(@user).not_to be_valid
+    @user.errors.full_messages
+  end
+  it 'is not valid when the password is shorter than 5 characters' do
+    @user = User.new(first_name: 'Rachel', last_name: 'Green', email: 'email@email.com', password: 'four', password_confirmation: 'four')
+    expect(@user).not_to be_valid
+    @user.errors.full_messages
+  end
   end
 end
-
-# It must be created with a password and password_confirmation fields
-# These need to match so you should have an example for where they are not the same
-# These are required when creating the model so you should also have an example for this
-# Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
-# Email, first name, and last name should also be required
