b337ba789d962c71a9701b5be98f2c4b6cdaf8bb
Jinny Jang <jangjinny21@gmail.com>
2021-03-09 22:38:07 +0000

    add test for product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,10 +16,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see product details for a specific product" do
     visit root_path
-
-    expect(page).to have_css 'article.product', count: 10
+    first('.product').click_link 'Details'
+    expect(page).to have_css '.products-show'
   end
-
 end
