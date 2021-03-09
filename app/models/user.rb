69e94b35dfc3b6393c69299b47c960815853d1de
Jinny Jang <jangjinny21@gmail.com>
2021-03-09 08:51:13 +0000

    create authentication method for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
